8baf33348c1347321096dd0f8287bd617df394ab
Jahanzaib <m.jahanzaib727@gmail.com>
2021-04-20 18:02:09 +0500

    radio button selector

--- src/components/twentyApril/nextTask/radioSelector.js
+++ src/components/twentyApril/nextTask/radioSelector.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import Select from "react-select";
+import Select, { components } from 'react-select';
 import './button.css';
 
 const options = [
-  { value: "Abe", label: "Agency (comming soon)", customAbbreviation: "Adv, connunication, event, media, pr, etc.." },
-  { value: "John", label: "Brand", customAbbreviation: "Apparel, accessories, eyewear, etc.." },
-  { value: "Dustin", label: "Dustin", customAbbreviation: "Restaurant, hotel, club, gym, beauty salon, etc.." },
-  { value: "Dustin", label: "Talent agency/Agent (coming soon..)", customAbbreviation: "Management, modellng, scouting," },
+  { value: "Agency", label: "Agency (comming soon)", customAbbreviation: "Adv, connunication, event, media, pr, etc.." },
+  { value: "Brand", label: "Brand", customAbbreviation: "Apparel, accessories, eyewear, etc.." },
+  { value: "local business", label: "Local Business", customAbbreviation: "Restaurant, hotel, club, gym, beauty salon, etc.." },
+  { value: "Talent Agent", label: "Talent agency/Agent (coming soon..)", customAbbreviation: "Management, modellng, scouting," },
 ];
 
-const formatOptionLabel = ({ value, label, customAbbreviation }) => (
-  <div style={{ display: "flex" }}>
-    <div className="content">
-       <h4>{label}</h4>
-       <p>{customAbbreviation}</p>
-    </div> 
-    <div>
-      <input type="radio" />
-    </div>
-  </div>
-);
+
+
+const MyOption = props => {
+  const { innerProps, innerRef } = props;
+  return (
+    <article className="content" ref={innerRef} {...innerProps} >
+        <div className="one">
+            <h4>{props.data.label}</h4>
+            <p>{props.data.customAbbreviation}</p>
+        </div>
+        <div className="two">
+          <input 
+          className="circle"
+          type="radio"
+          name='option'  
+          
+           />
+        </div>
+     </article>
+  );
+};
+
 
 
 class Radio extends React.Component {
-    state = { 
-      selectedOption: null,
-     };
-
-     handleChange = selectedOption => {
-      this.setState({ selectedOption });
-      console.log(`Option selected:`, selectedOption);
-    };
 
     render() { 
-      const { selectedOption } = this.state;
         return ( 
             <div className="radio">
                 <h3>Signing Up as</h3>
                 <Select className="radio-select"
-                  formatOptionLabel={formatOptionLabel}
+                  components={{Option: MyOption}}
                   options={options}
-                  value={selectedOption}
-                  onChange={this.handleChange}
+
                  />
             </div>
          );
