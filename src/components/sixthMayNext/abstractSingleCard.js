5812a352a6dc7e79aa403f61bfa5cc7fd187cb11
Jahanzaib <m.jahanzaib727@gmail.com>
2021-05-06 21:20:50 +0500

    change

--- src/components/sixthMayNext/abstractSingleCard.js
+++ src/components/sixthMayNext/abstractSingleCard.js
@@ -6,17 +6,17 @@ import styles from './sixMayCard.module.css';
     const {src, name, address,Icon1,followers,Icon2,actions, style, onClick} = {...props };
   return (
     
-            <div className={styles.abstractWrapper} onClick={onClick}>
-                 <div className={styles.abstarctDiv1} style={style} >
+            <div className={styles.abstractWrapper}>
+                 <div className={styles.abstarctDiv1} style={style}  onClick={onClick}>
                      <img className={styles.abstarctImg} src={src} />
                  </div>
 
-                 <div className={styles.abstarctDiv2} style={style} >
+                 <div className={styles.abstarctDiv2} style={style}  onClick={onClick}>
                    <p className={styles.name}>{name}</p>
                    <p className={styles.adress}>{address}</p>
                  </div>
 
-                 <div className={styles.abstarctDiv3} style={style} >
+                 <div className={styles.abstarctDiv3} style={style} onClick={onClick} >
                      <div>
                          <span className={styles.icon1}>{ Icon1 ? <Icon1/>: {Icon1} }</span>
                          <span className={styles.follower}>{followers}</span>
