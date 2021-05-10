788ecbc5f84dbfb3ee97e6f69ce50b384440c803
Jahanzaib <m.jahanzaib727@gmail.com>
2021-05-10 14:24:30 +0500

    tenMay

---
+++ src/components/tenMayGallery/GalleryModal.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react';
+import Modal from 'react-modal';
+import Carousel from 'react-images';
+import { Images } from './pics';
+import styles from './tenMay.module.css';
+ 
+
+
+function GalleryModal () {
+    const [currentImage, setCurrentImage] = useState(1);
+    const [modalIsOpen, setModalIsOpen] = useState(true);
+    return(
+        <div className={styles.tenMay}>
+            <Modal className={styles.modal} isOpen={modalIsOpen}>
+            <div className={styles.header}>
+                <div className={styles.headerDiv1}>
+                    <img src="./images/profile.jpg" />
+                    <div className={styles.headerDiv1sub}>
+                        <p className={styles.p1}>Tommothy Steward</p>
+                        <p className={styles.p2}>@vluebear</p>
+                    </div>
+                </div>
+                
+                <div>
+                    <p className={styles.p1}>May 10, 2020</p>
+                    <p className={styles.p2}>2:04 pm</p>
+                </div>
+            </div>
+
+            <div className={styles.carsoalDiv}>
+                <Carousel 
+                currentIndex={currentImage}
+                views={Images}
+                />
+            </div>
+
+            <div className={styles.buttonDiv}>
+                <button className={styles.btn1}>Reject</button>
+                <button className={styles.btn2}>Approved</button>
+            </div>
+
+            </Modal>
+        </div>
+    )
+}
+export default GalleryModal;
---
+++ src/components/tenMayGallery/pics.js
@@ -0,0 +1,33 @@
+export const Images = [
+    {
+      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      width: 1,
+      height: 1
+    },
+
+  ];
