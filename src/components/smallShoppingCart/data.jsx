ea581a766e4938d5cfdc192ae9ca280cd9993274
Jahanzaib <m.jahanzaib727@gmail.com>
2021-05-21 15:26:21 +0500

    Update data.jsx

--- src/components/smallShoppingCart/data.jsx
+++ src/components/smallShoppingCart/data.jsx
@@ -3,17 +3,17 @@ const data = {
         {id:'1',
           name: 'Mackbook',
           price: 14400,
-          image: './images/profile-3.jpg'
+          image: './images/oldCar.jpg'
     },
     {id:'2',
     name: 'Old Car',
     price: 34400,
-    image:  './images/profile-3.jpg'
+    image:  './images/mackbook.jpg'
 },
 {id:'3',
-name: 'Mackbook',
+name: 'Shoes',
 price: 900,
-image:  './images/profile-3.jpg'
+image:  './images/shoes.jpg'
 },
 
     ],
