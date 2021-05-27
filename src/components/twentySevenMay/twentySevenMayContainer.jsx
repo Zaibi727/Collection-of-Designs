db8e7945062642858806b0f420d5f4a354f0152a
Jahanzaib <m.jahanzaib727@gmail.com>
2021-05-27 15:16:08 +0500

    e

--- src/components/twentySevenMay/twentySevenMayContainer.jsx
+++ src/components/twentySevenMay/twentySevenMayContainer.jsx
@@ -43,7 +43,7 @@ class TwentySevenMayContainer extends React.Component{
     }
     componentWillUnmount() {
       document.removeEventListener("mousedown", this.handleClickOutside);
-      document.addEventListener("mousedown", this.handleRClickOutside);
+      document.removeEventListener("mousedown", this.handleRClickOutside);
     }
 
     handleClickOutside = event => {
@@ -82,39 +82,39 @@ class TwentySevenMayContainer extends React.Component{
     return (
         <div className={styles.wrapper}>
             <header className={styles.header}>
-                <div className={styles.left} style={{backgroundColor: this.state.bgColor}}>
+                <div  ref={this.container} className={styles.left} style={{backgroundColor: this.state.bgColor}}>
                     <button className={styles.btnLeft} onClick={this.handleButtonClick} ><GiHamburgerMenu /></button>
 
                     {this.state.open && (
                         <div className={styles.leftAside}>
                              <h4>Left header</h4> 
                                      
-                            <aside>
+                            <div>
                                <h4>Left Content</h4>
                                <p>
                                Nam accumsan eleifend metus at imperdiet. Mauris pellentesque ipsum nisi, et fringilla leo blandit sed. In tempor, leo sit amet fringilla imperdiet, ipsum enim sagittis sem, non molestie nisi purus consequat sapien. Proin at velit id elit tincidunt iaculis ac ac libero. Vivamus vitae tincidunt ex. Duis sit amet lacinia massa. Quisque lobortis tincidunt metus ut commodo. Sed euismod quam gravida condimentum commodo.
                                 Vivamus tincidunt risus ut sapien tincidunt, ac fermentum libero dapibus. Duis accumsan enim ac magna tempor, vestibulum euismod nisl pharetra. Ut dictum lacus eu venenatis vestibulum. Vestibulum euismod at arcu ac blandit. Curabitur eu imperdiet magna. Duis bibendum efficitur diam, eget placerat nunc imperdiet eget. Morbi porta at leo sed porta. Nullam eleifend eleifend quam eget dictum.
                                </p>
-                           </aside>
+                           </div>
                              </div>
                      )}
                 </div>
 
                 <h4>Main header</h4>
 
-                <div className={styles.right} style={{backgroundColor: this.state.bgColor}}>
+                <div  ref={this.container} className={styles.right} style={{backgroundColor: this.state.bgColor}}>
                     <button className={styles.btnRight} onClick={this.handleRButtonClick}>  <GiHamburgerMenu />  </button>
                     {this.state.opene && (  
                              <div className={styles.rightAside}>
                              <h4>Right header</h4> 
                                      
-                             <aside>
+                             <div>
                                <h4>Right Content</h4> 
                                <p>
                                Nam accumsan eleifend metus at imperdiet. Mauris pellentesque ipsum nisi, et fringilla leo blandit sed. In tempor, leo sit amet fringilla imperdiet, ipsum enim sagittis sem, non molestie nisi purus consequat sapien. Proin at velit id elit tincidunt iaculis ac ac libero. Vivamus vitae tincidunt ex. Duis sit amet lacinia massa. Quisque lobortis tincidunt metus ut commodo. Sed euismod quam gravida condimentum commodo.
                                 Vivamus tincidunt risus ut sapien tincidunt, ac fermentum libero dapibus. Duis accumsan enim ac magna tempor, vestibulum euismod nisl pharetra. Ut dictum lacus eu venenatis vestibulum. Vestibulum euismod at arcu ac blandit. Curabitur eu imperdiet magna. Duis bibendum efficitur diam, eget placerat nunc imperdiet eget. Morbi porta at leo sed porta. Nullam eleifend eleifend quam eget dictum.
                                </p>
-                           </aside>
+                           </div>
                              </div>
                      )}
                 </div>
