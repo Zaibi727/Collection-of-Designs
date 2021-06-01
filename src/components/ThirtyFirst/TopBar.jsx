22899a1dbaaa397b25e99b4fcb92e1c0d02bd1b6
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-01 16:37:12 +0500

    Update TopBar.jsx

--- src/components/ThirtyFirst/TopBar.jsx
+++ src/components/ThirtyFirst/TopBar.jsx
@@ -34,7 +34,6 @@ class TopBar extends Component {
             openee: false,
             openeee: false,
             openeeee: false,
-            selectedOption: {  value: 'chocolate', label: <div><img src="./images/flag1.png" height="20px" width="30px" />En</div>},
          };
 
          this.handleButtonOneClick = this.handleButtonOneClick.bind(this);
