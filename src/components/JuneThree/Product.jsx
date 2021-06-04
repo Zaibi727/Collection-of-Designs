664ffc62487a96803d6e32223eff74109a334f0b
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-04 11:22:26 +0500

    Update Product.jsx

--- src/components/JuneThree/Product.jsx
+++ src/components/JuneThree/Product.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import styles from './juneThree.module.css';
-import sideItems from './sidedata';
 import {Link} from 'react-router-dom';
-
+import sideItems from './sidedata';
 
 class Product extends React.Component {
 
 
-  render(){   
-
+    render(){
       return (
-        <div className={styles.product}>
-        {sideItems.map((item, Icon,) => (
+           <div className={styles.product}>
+              {sideItems.map((item) => (
              <ul className={styles.ul} key={item.itemid}>
-             <Link className={styles.link} to={`/product/${item.itemid}`}>
-               <li><span>{item.Icon}</span>{item.label}</li>  
-             </Link>
+                   <Link className={styles.link} to={`/product/${item.itemid}`}>
+                         <li><span>{item.Icon}</span>{item.label}</li>  
+                    </Link>
+                    <li>
+                        {item.content.map((c) => (
+                         <ul>
+                              <li>{c.label}<span>{c.Icon2}</span></li>
+                         </ul>
+                         ))}
+             </li>
               </ul>
         ))}
-        </div>
+          </div> 
      );
 }
 }
