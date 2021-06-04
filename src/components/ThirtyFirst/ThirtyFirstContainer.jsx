f1659891fafca96b0166dfd29c0fad19d843ffd1
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-04 15:38:01 +0500

    e

--- src/components/ThirtyFirst/ThirtyFirstContainer.jsx
+++ src/components/ThirtyFirst/ThirtyFirstContainer.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import TopBar from './TopBar';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 class ThirtyFirstContainer extends Component {
     state = {  }
     render() { 
         return ( 
-            <div>
+            <div className={styles.container}>
+              <Router>
+              <div className={styles.up}>
                 <TopBar />
+              </div>
+
+              <Route path="/:id" />
+              </Router>
             </div>
          );
     }
