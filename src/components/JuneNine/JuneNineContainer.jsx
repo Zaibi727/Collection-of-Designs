982a0ddccce20a266963044c8aef2f63811b3fcb
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-10 18:34:44 +0500

    Update JuneNineContainer.jsx

--- src/components/JuneNine/JuneNineContainer.jsx
+++ src/components/JuneNine/JuneNineContainer.jsx
@@ -1,93 +1,87 @@
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: content-box;
-}
-.container{
-  width: 100%;
-  height: 100vh;
-  overflow: auto;
-  display: flex;
-  flex-direction: column;
-}
-.wrapper{
-  width: 100%;
-  display: grid;
-  grid-template-columns: 20% 80%;
-  height: 100%;
-  background-color: rgb(214, 212, 214);
-}
-.buttonportion{
-  background-color: rgb(255, 255, 255);
-  padding: 10px 7px 0px 0px;
-border-bottom:1px solid rgb(190, 187, 187);
-height: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-}
-.buttonportion:active{
-  background-color: #2fc772;
-  color: white;
-}
-
-.cardPortion{
-  position: fixed;
-  left: 40%;
-  top: 30%;
-  bottom: 100%;
-  background-color: forestgreen !important;
-}
-.aaa{
-  font-size: 25px;
-}
-.bbb{
-  font-size: 16px;
-  font-weight: 500;
-}
-
-/*--container---*/
-
-.abcard{
-  width: 350px;
-  height: 160px;
-  background-color: rgb(255, 255, 255);
-}
+import React,{Component} from 'react';
+import styles from './junenine.module.css';
+import datas from './cardData';
+//import AbButton from '../twentyMay/abButton';
+import AbCard from './AbCard'
+import AbCardContent from './AbCardContent';
 
 
-/*--Abcard--*/
+class JuneNineContainer extends Component {
+    constructor(props) {
+      super(props);
+     
+      this.state = { 
+          open: false,
+          selected: '',
+          
+       };
 
-.headerdiv{
-  margin-bottom: 20px;
-  background-color: #2fc772;
-}
-.headerdiv p{
-  padding: 5px;
-  color: white;
-  font-weight: 500;
-}
-.subcontentwrapper{
-  display: flex;
-  flex-wrap: wrap;
-  width: 300px;
-  margin-left: 15%;
-}
-.AbCardContent{
-  display: flex;
-  align-items: flex-start;
-  text-align: left;
-  margin-right: 20px;
-}
-.ps{
-  margin-left: 10px;
-  font-size: 13px;
+       this.wrapperRef = React.createRef();
+       this.OneClick = this.OneClick.bind(this);    
+       this.OutsideOne = this.OutsideOne.bind(this);
+     
+  }
+  
+  
+  OneClick = (key) => {
+      this.setState(state => {
+        return {
+          open: !state.open, 
+          selected: key,
+        };
+      });
+    };
  
+    
+
+  
+    componentDidMount() {
+      document.removeEventListener("mousedown", this.OutsideOne);
+
+  }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.OutsideOne);
+
+  }
+  
+  OutsideOne = event => {
+      if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
+        this.setState({
+          open: false,
+          selected: '',
+        });
+      }
+    };
+
+
+render(){
+    return (
+        <div className={styles.container}>
+             {datas.map((data, i) => {
+               return(
+                <div className={styles.wrapper} key={i}>
+                   <div  className={styles.buttonportion} onClick={() => this.OneClick(i)}>
+                      <p className={styles.aaa}>{data.Icon}</p>
+                      <p className={styles.bbb}>{data.label}</p>
+                   </div>
+
+                 {data.cardContent ? data.cardContent.map((c) => 
+                              <div className={styles.cardPortion}>  
+                              {this.state.open && this.state.selected === i && (
+                                <AbCard>                
+                                   <AbCardContent 
+                                     c={c}
+                                   />
+                                </AbCard>
+                              )}
+                              </div>                       
+                            ) : null } 
+
+               </div>
+               )})}
+              
+        </div>
+    )
 }
-.p1{
-  margin-bottom: 5px;
-  font-weight: 500;
 }
-.p2{
-  margin-top: -5px;
-  font-size: 13px;
-}
+export default JuneNineContainer;
