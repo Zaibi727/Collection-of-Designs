ac635af8f29dcde4bb08e549e9713890817ed4cc
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-10 19:46:36 +0500

    c

--- src/components/JuneNine/JuneNineContainer.jsx
+++ src/components/JuneNine/JuneNineContainer.jsx
@@ -13,61 +13,42 @@ class JuneNineContainer extends Component {
       this.state = { 
           open: false,
           selected: '',
-          
+        
        };
 
-       this.wrapperRef = React.createRef();
        this.OneClick = this.OneClick.bind(this);    
-       this.OutsideOne = this.OutsideOne.bind(this);
      
   }
   
   
-  OneClick = (key) => {
+  OneClick = (id) => {
       this.setState(state => {
         return {
           open: !state.open, 
-          selected: key,
+          selected: id,
         };
       });
     };
  
     
+ 
 
-  
-    componentDidMount() {
-      document.removeEventListener("mousedown", this.OutsideOne);
-
-  }
-  componentWillUnmount() {
-    document.removeEventListener("mousedown", this.OutsideOne);
-
-  }
-  
-  OutsideOne = event => {
-      if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
-        this.setState({
-          open: false,
-          selected: '',
-        });
-      }
-    };
 
 
 render(){
     return (
         <div className={styles.container}>
-             {datas.map((data, i) => {
+             {datas.map((data, id) => {
                return(
-                <div className={styles.wrapper} key={i}>
-                   <div  className={styles.buttonportion} onClick={() => this.OneClick(i)}>
+                <div  className={styles.wrapper} key={id} ref={this.container}>
+                   <div  className={styles.buttonportion} onClick={() => this.OneClick(id)}  style={{color: this.state.selected === id ? 'white' : 'black', backgroundColor: this.state.selected === id ? '#2fc772' : 'white'}}>
                       <p className={styles.aaa}>{data.Icon}</p>
                       <p className={styles.bbb}>{data.label}</p>
                    </div>
 
                  {data.cardContent ? data.cardContent.map((c) => 
                               <div className={styles.cardPortion}>  
-                              {this.state.open && this.state.selected === i && (
+                              {this.state.open && this.state.selected === id && (
                                 <AbCard>                
                                    <AbCardContent 
                                      c={c}
