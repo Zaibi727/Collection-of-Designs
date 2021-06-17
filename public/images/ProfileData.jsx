ccef3866646e7a39f7798e54217cf2b9b2689d77
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-17 20:20:14 +0500

    images

---
+++ public/images/ProfileData.jsx
@@ -0,0 +1,36 @@
+const ProfileData = {
+    datas: [
+        {
+           
+            name: 'Arlene Steward',
+            image: './images/profile-3.jpg',
+        },
+        {
+            
+            name: 'Serenity',
+            image: './images/eight-1.jpg',
+        },
+        {
+           
+            name: 'Bessie Flores',
+            image: './images/eight2.jpg',
+        },
+        {
+            
+            name: 'Cameron Black',
+            image: './images/eight3.jpg',
+        },
+        {
+            
+            name: 'Debra Simmmons',
+            image: './images/eight5.jpg',
+        },
+        {
+           
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+        },
+    ]
+}
+
+export default ProfileData;
