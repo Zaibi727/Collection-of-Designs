855c432d618ad52546f7ca34b602082679105f24
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-17 17:15:55 +0500

    e

--- src/components/Modal/AbstarctCard.jsx
+++ src/components/Modal/AbstarctCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './Modal.module.css';
 
-export default function AbstarctCard() {
+export default function AbstarctInput({label, value, checked, onChange}) {
     return (
-        <div className={styles.abCard}>
-            
+        <div className={styles.ab}>
+             <div>
+                 <label className={styles.labelcover}>
+                               <p>
+                                 {label}
+                               </p>
+                               <input 
+                               className={styles.circle}
+                                type="radio"  
+                                value={value}
+                                checked={checked}
+                                onChange={onChange}
+                                />
+                 </label>
+            </div>
         </div>
     )
 }
