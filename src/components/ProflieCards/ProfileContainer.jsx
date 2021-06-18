685b04182a6ba7060788c915822f16aee4425f43
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-18 13:30:01 +0500

    d

--- src/components/ProflieCards/ProfileContainer.jsx
+++ src/components/ProflieCards/ProfileContainer.jsx
@@ -4,6 +4,9 @@ import ProfileData from './ProfileData';
 import { BsDot } from "react-icons/bs";
 import { FiUsers } from "react-icons/fi";
 
+
+
+
 class ProfileContainer extends Component {
 	constructor(props) {
 		super(props);
@@ -47,10 +50,10 @@ class ProfileContainer extends Component {
                   <p className={styles.p1}>12:00 - 15:00 <BsDot /> <span><FiUsers /> 8</span></p>
                   <div className={styles.btnPanelWrapper}>
                 <div className={styles.buttonPanel}>        
-                    {ProfileData.map((data, index) => (
+                {ProfileData.map((data, index) => (
                         <div  key={index} className={styles.btnrow}>
                             <div className={styles.buttonPanel} onClick={() => this.OneClick(data.id)} >
-                                <div onClick={() => this.OneClick(data.id)} style={{ fontWeight: data.id === this.state.selected ? '700' : '400', borderBottom: data.id === this.state.selected ? '4px solid lightgray' : 'white' }}>
+                                <div onClick={() => this.OneClick(data.id)} style={{ fontWeight: data.id === this.state.selected ? '700' : '400' }}>
                                    <img className={styles.img} src={data.image}  style={{border: data.id === this.state.selected ? '1px solid black' : 'none', padding: data.id === this.state.selected ? '2px' : '0px'}}/>                               
                                    <div>
                                       <p className={styles.name} style={{ color: data.id === this.state.selected ? 'black' : 'gray' }}>{data.name}</p>
@@ -59,7 +62,7 @@ class ProfileContainer extends Component {
                                 </div>
                             </div>
                         </div>
-                    ))}
+                    ))} 
                 </div>
                 </div>
                 </div>
