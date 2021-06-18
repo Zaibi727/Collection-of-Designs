0137e6a26886efa70e7199cd9b61f820d5bb1dd2
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-18 13:32:52 +0500

    Update ProfileData.jsx

--- src/components/ProflieCards/ProfileData.jsx
+++ src/components/ProflieCards/ProfileData.jsx
@@ -73,6 +73,150 @@ const ProfileData = [
             image: './images/eight7.jpg',
             Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
         },
+        {
+            id:"13",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"14",
+            name: 'Cameron Black',
+            image: './images/eight3.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"15",
+            name: 'Debra Simmmons',
+            image: './images/eight5.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"16",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"17",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"18",
+            name: 'Cameron Black',
+            image: './images/eight3.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"19",
+            name: 'Debra Simmmons',
+            image: './images/eight5.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"20",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"21",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"22",
+            name: 'Cameron Black',
+            image: './images/eight3.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"23",
+            name: 'Debra Simmmons',
+            image: './images/eight5.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"24",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"25",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"26",
+            name: 'Cameron Black',
+            image: './images/eight3.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"27",
+            name: 'Debra Simmmons',
+            image: './images/eight5.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"28",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"29",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"30",
+            name: 'Cameron Black',
+            image: './images/eight3.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"31",
+            name: 'Debra Simmmons',
+            image: './images/eight5.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"33",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"34",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"35",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"36",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
+        {
+            id:"37",
+            name: 'Brandon Wat',
+            image: './images/eight7.jpg',
+            Iconone: <GoPrimitiveDot  style={{color: 'transparent'}}/>
+        },
     ]
 
 export default ProfileData;
